06815aa7e6e155c97f76da09bda2bcefdcc6684b
airemcs <its.airemcs@gmail.com>
2025-09-15 12:57:48 +0800

    Create Navbar Component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+export default function Navbar() {
+  return (
+  <div className="navbar">
+
+  <div className="navbar-start lg:ml-4">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+      <ul className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow font-semibold" tabIndex={0}>
+        <li><a>View Dashboard</a></li>
+        <li><a>Submission Form</a></li>
+        <li><a>Annotation Tool</a></li>
+        <li><a>About Us</a></li>
+      </ul>
+    </div>
+    <a className="text-xl font-bold">AEDA Studio</a>
+  </div>
+
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1 gap-x-8 font-semibold">
+      <li><a>View Dashboard</a></li>
+      <li><a>Submission Form</a></li>
+      <li><a>Annotation Tool</a></li>
+      <li><a>About Us</a></li>
+    </ul>
+  </div>
+  <div className="navbar-end lg:mr-4">
+    <a className="btn">Login</a>
+  </div>
+  </div>
+  )
+}
